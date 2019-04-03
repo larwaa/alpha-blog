2f1cb9a2f038309c0532c76302d3de3b8234ef98
Lars Waage <lars.waage@outlook.com>
2019-04-03 15:55:00 +0200

    added admin capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update, :show]
+	before_action :set_user, only: [:edit, :update, :show, :destroy]
 	before_action :require_same_user, only: [:edit, :update]
+	before_action :require_admin, only: [:destroy]
 
 	def new
 		@user = User.new
@@ -8,10 +9,10 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
-
 		if @user.save
-			flash[:success] = "Welcome to Bindeleddet #{@user.username}"
-			redirect_to articles_path
+			session[:user_id] = user_params[:user_id]
+			flash[:success] = "Welcome to Bindeleddet #{user_params[:username]}"
+			redirect_to root_path
 		else
 			render "new"
 		end
@@ -38,6 +39,12 @@ class UsersController < ApplicationController
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
 
+	def destroy
+		if @user.destroy
+			flash[:danger] = "#{@user.username} and all articles created by #{@user.username} have been deleted"
+		end
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
@@ -48,8 +55,15 @@ class UsersController < ApplicationController
 	end
 
 	def require_same_user
-		if !logged_in? or current_user != @user
+		if current_user != @user and !current_user.admin?
 			flash[:danger] = "You can only edit your own profile!"
+			redirect_to root_path
+		end
+	end
+
+	def require_admin
+		if logged_in? and !current_user.admin?
+			flash[:danger] = "Only admins can perform that action"
 			redirect_to root_path
 		end
 	end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 	def image_for(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-		image_tag(gravatar_url, alt: user.username, class: "card-img")
+		image_tag(gravatar_url, alt: user.username, class: "card-img", style: "object-fit: cover; height: 100%;")
 	end
 end
